aa7a5d32792b23c3e4aa89edff3cd4c0d342b9df
Jake Bogan <jakebogan01@gmail.com>
2023-04-26 17:12:16 -0400

    started basic database structor

---
+++ src/routes/headphones/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+     import db from "../../stores/ecommerceStore";
+</script>
+
+<div>
+     {#each $db as product}
+          {#if product?.title === "headphones"}
+               {#each product?.products as item (item?.id)}
+                    <a href="/headphones/{item?.slug}" class="block">{item?.title}</a>
+               {/each}
+          {/if}
+     {/each}
+</div>
---
+++ src/routes/headphones/[slug]/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+     import Cart from "../../../components/Cart.svelte";
+     import db from "../../../stores/ecommerceStore";
+     export let data;
+
+     const addProductToCart = () => {
+          //add item to cart
+     }
+ </script>
+ 
+ <h1>{data?.slug}</h1>
+ <br>
+ <div>
+     {#each $db as product}
+          {#if product?.title === "headphones"}
+               {#each product?.products as item (item?.id)}
+                    {#if item?.slug === data?.slug}
+                         <p>{item?.title}</p>
+
+                         <button type="button" class="bg-blue-300" on:click={()=>{addProductToCart()}}>purchase</button>
+                    {/if}
+               {/each}
+          {/if}
+     {/each}
+ </div>
+ <!-- <Cart /> -->
