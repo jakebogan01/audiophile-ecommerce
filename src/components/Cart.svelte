35ddf1a61a09b01dc1f295a68e9277dbc6166cf2
Jake Bogan <jakebogan01@gmail.com>
2023-04-27 02:20:29 -0400

    got checkout to showcase purchased items

--- src/components/Cart.svelte
+++ src/components/Cart.svelte
@@ -1,5 +1,39 @@
 <script>
-     
+     import { onMount } from 'svelte';
+     import { preferences } from "/src/stores/ecommerceStore";
+
+     let products = [];
+
+     onMount(() => {
+          $preferences.find(data => data.title === "headphones").products.map(
+          (product) => {
+               if (product.purchased) {
+                    products.push(product);
+               }
+          });
+          $preferences.find(data => data.title === "speakers").products.map(
+          (product) => {
+               if (product.purchased) {
+                    products.push(product);
+               }
+          });
+          $preferences.find(data => data.title === "earphones").products.map(
+          (product) => {
+               if (product.purchased) {
+                    products.push(product);
+               }
+          });
+
+          products = [...products];
+     })
+
 </script>
 
 <h1>Cart</h1>
+<br>
+<div>
+     {#each products as item}
+          <p>{item?.title}</p>
+          <p>{item?.price}</p>
+     {/each}
+</div>
--- src/routes/headphones/[slug]/+page.svelte
+++ src/routes/headphones/[slug]/+page.svelte
@@ -2,12 +2,15 @@
      import { preferences } from "../../../stores/ecommerceStore";
      export let data;
 
-     const addProductToCart = () => {
+     const addProductToCart = (id) => {
           preferences.update(currentData => {
                let copiedData = [...currentData];
-               let updatedData = copiedData.find(data => data.title === "cart");
-               updatedData.products = [...updatedData.products, {title: "hanging"}];
+               let updatedData = copiedData.find(data => data.title === "headphones");
+               let productData = updatedData.products.find(product => product.id === id);
                
+               productData.purchased = true;
+               productData.quantity = 1;
+
                return copiedData;
           });
      }
@@ -22,7 +25,7 @@
                     {#if item?.slug === data?.slug}
                          <p>{item?.title}</p>
 
-                         <button type="button" class="bg-blue-300" on:click={()=>{addProductToCart()}}>purchase</button>
+                         <button type="button" class="bg-blue-300" on:click={()=>{addProductToCart(item?.id)}}>purchase</button>
                     {/if}
                {/each}
           {/if}
