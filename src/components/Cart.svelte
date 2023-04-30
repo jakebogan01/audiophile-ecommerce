3b1f9be870b2242e6621f05a7c180cda8cd054d8
Jake Bogan <jakebogan01@gmail.com>
2023-04-30 18:14:43 -0400

    finshed cart styles

--- src/components/Cart.svelte
+++ src/components/Cart.svelte
@@ -77,11 +77,11 @@
 </div> -->
 
 {#if show}
-     <div class="relative z-50 font-manrope" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-          <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-          <div class="fixed inset-0 z-10 overflow-y-auto">
-               <div class="flex min-h-full items-start justify-center pt-[7.125rem] px-6 text-center sm:items-center sm:p-0" on:keydown={()=>{}} on:click|self>
-                    <div class="relative transform overflow-hidden rounded-lg bg-white w-full px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+     <div class="relative z-40 font-manrope" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+          <div class="fixed inset-0 bg-black bg-opacity-50 transition-opacity"></div>
+          <div class="fixed inset-0 z-10 overflow-y-auto" on:keydown={()=>{}} on:click|self>
+               <div class="flex min-h-full items-start justify-center md:justify-end pt-[7.125rem] max-w-[69.375rem] mx-auto px-6 md:px-2 text-center" on:keydown={()=>{}} on:click|self>
+                    <div class="relative transform overflow-hidden rounded-lg bg-white w-full max-w-[23.5625rem] px-4 pb-4 pt-5 md:px-[2.0625rem] md:py-6 text-left shadow-xl transition-all">
                          
                          <div class="mx-auto max-w-2xl">
                               <div class="flex justify-between items-center">
@@ -99,7 +99,7 @@
                                              {#each products as product}
                                                   <li class="flex items-center py-6">
                                                        <div class="flex-shrink-0">
-                                                            <img src={product?.cart} alt="Front side of mint cotton t-shirt with wavey lines pattern." class="h-16 w-16 rounded-md object-cover object-center sm:h-32 sm:w-32">
+                                                            <img src={product?.cart} alt="Front side of mint cotton t-shirt with wavey lines pattern." class="h-16 w-16 rounded-md object-cover object-center">
                                                        </div>
 
                                                        <div class="ml-4 flex justify-between flex-1">
@@ -134,7 +134,7 @@
                                         </div>
 
                                         <div class="mt-6">
-                                             <a href="/checkout" class="flex justify-center items-center w-full bg-dark-orange px-4 h-12 text-[0.8125rem] font-bold text-white shadow-sm focus:outline-none">Checkout</a>
+                                             <a href="/checkout" on:click|self class="flex justify-center items-center w-full bg-dark-orange px-4 h-12 text-[0.8125rem] font-bold text-white shadow-sm focus:outline-none">Checkout</a>
                                         </div>
                                    </section>
                               </form>
